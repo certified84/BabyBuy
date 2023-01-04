ca2ff3bd9d417c2a6d28742afb7bd2416be45cb8
Samson Achiaga <sirhumble84@gmail.com>
2023-01-04 03:59:39 +0100

    Add LoginFragment.kt

--- app/src/main/java/com/certified/babybuy/ui/auth/OnboardingFragment.kt
+++ app/src/main/java/com/certified/babybuy/ui/auth/OnboardingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.certified.babybuy.R
 import com.certified.babybuy.databinding.FragmentOnboardingBinding
 
@@ -20,6 +21,16 @@ class OnboardingFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnLogin.setOnClickListener {
+//                findNavController().navigate(OnboardingFragmentDirections.actionOnboardingFragmentToLoginFragment())
+            }
+        }
     }
 
     override fun onDestroyView() {
