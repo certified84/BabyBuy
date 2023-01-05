cbe91bcf6a03523c323a0266f282ca8b3834f8ca
Samson Achiaga <sirhumble84@gmail.com>
2023-01-05 23:25:33 +0100

    Add firebase and hilt

---
+++ app/src/main/java/com/certified/babybuy/di/BabyBuyApp.kt
@@ -0,0 +1,7 @@
+package com.certified.babybuy.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BabyBuyApp: Application()
---
+++ app/src/main/java/com/certified/babybuy/di/NetworkModule.kt
@@ -0,0 +1,17 @@
+package com.certified.babybuy.di
+
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebase(): Firebase = Firebase
+}
